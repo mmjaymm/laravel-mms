f5a4933c436cd49f712017c34be7211e34ae14ff
Mark Jay <markjaymercado@ph.fujitsu.com>
2019-11-22 13:56:12 +0800

    [ATTENDANCE] *Created : model function insert data *Added : route insert

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('attendances', 'AttendanceController@show');
+Route::get('attendances/insert', 'AttendanceController@store');
