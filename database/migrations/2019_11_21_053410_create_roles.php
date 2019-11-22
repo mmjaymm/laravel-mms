e0919231f7dde377212d0f23f18fbfc619e00b77
Mark Jay <markjaymercado@ph.fujitsu.com>
2019-11-22 14:59:02 +0800

    [DATABASE] *Added : roles timestamp carbon

--- database/migrations/2019_11_21_053410_create_roles.php
+++ database/migrations/2019_11_21_053410_create_roles.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Carbon;
 use App\Role;
 
 class CreateRoles extends Migration
@@ -18,10 +19,13 @@ class CreateRoles extends Migration
             $table->bigIncrements('id');
             $table->string('level');
             $table->timestamps();
-
         });
 
-        Role::insert(['level' => 'USER']);
+        Role::insert([
+            'level' => 'USER',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 
     /**
