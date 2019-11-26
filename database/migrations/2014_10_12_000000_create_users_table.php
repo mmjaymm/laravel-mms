7444c7d5c78da521aed9e4f33dbf08b2872f05ea
JEFFERSON GERNALE <fp190282@fdtp.local>
2019-11-26 10:39:49 +0800

    [MIGRATION] - Update: rename migration filename

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,11 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Carbon;
 
-use App\Hris;
-use App\User;
 
 class CreateUsersTable extends Migration
 {
@@ -30,30 +26,6 @@ class CreateUsersTable extends Migration
             $table->foreign('roles_id')->references('id')->on('roles');
         });
 
-        //getting man power of MIT in HRIS
-        $hris = new Hris;
-        $where = [
-            ['section_code', 'MIT'],
-            ['emp_system_status', 'ACTIVE']
-        ];
-        $man_power_result = $hris->man_power($where);
-        $users = [];
-
-        foreach ($man_power_result as $key => $user) {
-            $first_name = str_replace(" ", "", strtolower($user->emp_first_name));
-            $last_name = str_replace(" ", "", strtolower($user->emp_last_name));
-
-            array_push($users, [
-                'employee_number' => $user->emp_pms_id,
-                'email' => "{$first_name}.{$last_name}@ph.fujitsu.com",
-                'password' => Hash::make('fp'.$user->emp_pms_id),
-                'roles_id' => 1,
-                'created_at'        => Carbon::now(),
-                'updated_at'        => Carbon::now(),
-            ]);
-        }
-        
-        User::insert($users);
     }
 
     /**
