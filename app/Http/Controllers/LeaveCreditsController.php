e11bf7f7c90e93760500197d5cac6c26ca3b0104
Erika Reformado <erika.reformado@fujitsu.ph.com>
2019-11-28 11:00:29 +0800

    [LEAVE] Added validation on store function

--- app/Http/Controllers/LeaveCreditsController.php
+++ app/Http/Controllers/LeaveCreditsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\LeaveCredits;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 
 class LeaveCreditsController extends Controller
@@ -18,12 +19,26 @@ class LeaveCreditsController extends Controller
   
     public function store(Request $request)
     {
+        $date_from = $request->date_from;
+        $date_to = $request->date_to;
+        $leave_type_id = $request->leave_type_id;
+        $users_id = $request->users_id;
         $data = $request->except('_token');
+        $message = [
+            'date_from.unique'=> 'You already set leave credits in this user.'
+        ];
         $validator = Validator::make($request->all(),[
-            'leave_type_id' => 'required',
+            'leave_type_id' =>'required',
             'credits' => 'required',
             'users_id' => 'required',
-        ]);
+            'date_from' => ['required',
+            Rule::unique('leave_credits')->where(function ($query) use ($date_from, $date_to, $leave_type_id,$users_id) {
+                return $query->whereBetween('date_from',[$date_from, $date_to])
+                             ->where('leave_type_id',$leave_type_id)
+                             ->where('users_id', $users_id);
+            })],
+            'date_to' => 'required',
+        ],$message);
         if($validator->fails())
         {
             return response()->json($validator->errors()->all()); 
