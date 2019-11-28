378aab03b0a7f1eeedd878848c7aa154c4c67034
Arniel Casile <arniel.casile@ph.fujitsu.com>
2019-11-28 15:39:23 +0800

    [FIXED CONFLICT MERGE]Fixed: The conlict during merge in local

--- app/Http/Controllers/FailureController.php
+++ app/Http/Controllers/FailureController.php
@@ -9,10 +9,8 @@ use App\Http\Requests\FailurePost;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Carbon;
 
-
 class FailureController extends Controller
 {
-
     private function datas($data)
     {   
         $data->except('_token');
@@ -25,18 +23,25 @@ class FailureController extends Controller
             'users_id'          => $data->users_id
         ];
     }
-
     public function index()
     {
         return csrf_token();
+
     }
+
+
+    public function create()
+    {
+        
+    }
+
 
     /*
     * return @array
     * request data required [ id,datetime_out, datetime_in, reason, attendances_id]
     */
 
-    public function create(FailurePost $input_request, Failure $failures)
+    public function store(FailurePost $input_request, Failure $failures)
     {
 
         $failures = new Failure();
@@ -53,55 +58,46 @@ class FailureController extends Controller
 
         DB::beginTransaction(); 
 
-        try {
-            //insert failures
+        $failures->insert_failure_data($this->datas($input_request));   
+        DB::commit();
+
+        // try {
+        //     //insert failures
             
  
-            $failures->insert_failure_data($this->datas($input_request));   
-            $attendance_data = [
-                'status' => 'FAILURE'
-            ];
-            //update status in attendance
-            $attendances->update_data($input_request->attendances_id, $attendance_data) ;
-            DB::commit();
+        //     $failures->insert_failure_data($this->datas($input_request));   
+        //     $attendance_data = [
+        //         'status' => 'FAILURE'
+        //     ];
+        //     //update status in attendance
+        //     $attendances->update_data($input_request->attendances_id, $attendance_data) ;
+        //     DB::commit();
 
-            $return['result'] = TRUE;
-            $return['messages'] = 'Inserted Successfully.';
+        //     $return['result'] = TRUE;
+        //     $return['messages'] = 'Inserted Successfully.';
 
-        } catch (\Throwable $th) {
-            DB::rollback();
+        // } catch (\Throwable $th) {
+        //     DB::rollback();
 
-            $return['result'] = FALSE;
-            $return['messages'] = 'Unable to Insert';
-        }
+        //     $return['result'] = FALSE;
+        //     $return['messages'] = 'Unable to Insert';
+        // }
         
         return response()->json($return);
 
 
     }
 
-    /*
-    * return @array
-    * request data required [ id]
-    */
-    public function edit($id, Failure $failure)
-    {
-        $failure_data = $failure->edit_data($id);
 
-        if(count($failure_data) > 0)
-        {
-            $return['result'] = TRUE;
-            $return['data'] = $failure_data[0];
-            $return['messages'] = 'Data Found.';
-        }
-        else
-        {
-            $return['result'] = FALSE;
-            $return['data'] = [];
-            $return['messages'] = 'No Data Found.';   
-        }
+    public function show($id)
+    {
         
-        return response()->json($return);
+    }
+
+    public function edit($id)
+    {
+        $failures = new Failure();
+        return $failures->retrieve_one($id);      
     }
 
 
@@ -139,24 +135,8 @@ class FailureController extends Controller
     * return @array
     * request data required [ id]
     */
-
-    public function delete($id, Failure $failures)
+    public function destroy($id)
     {
-        $delete_result = $failures->update_data($id, ['is_deleted' => 1]);
-
-        if($delete_result)
-        {
-            $return['result'] = TRUE;
-            $return['messages'] = 'Deleted Successfully';
-        }
-        else
-        {
-            $return['result'] = FALSE;
-            $return['messages'] = 'Unabled to Delete.';   
-        }
-        
-        return response()->json($return);
+        //
     }
-
 }
-  
